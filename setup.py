cf15ac9653b40eac59b2e094574f0c3f3e9fb296
ifanmathonwy <eoindmahon@yahoo.co.uk>
2018-08-05 17:07:35 +0100

    Add packages.

--- setup.py
+++ setup.py
@@ -3,4 +3,5 @@ from setuptools import setup
 setup(name='eliot',
       version='0.1',
       description='A poetry generation library.',
+      packages=['eliot'],
       install_requires=['nltk', 'pronouncing'])
